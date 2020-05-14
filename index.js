3d49baa95eafd9ab8aad3cd036080ba584b1e79c
shna rafeeq <shnar.majeed@gmail.com>
2020-05-14 18:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("text").innerHTML = "";
+  document.getElementById("text").innerHTML = "This is really cool!";
 });
  
 console.log(
